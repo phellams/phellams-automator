34a6abd0aedce92930b0e53a00386fff652f4819
sgkens <sgkens@gmail.com>
2026-08-20 20:13:17 +1000

    docs(readme): update current image versions

--- phellams-automator-local-builder.ps1
+++ phellams-automator-local-builder.ps1
@@ -1,6 +1,6 @@
 param (
     [Parameter(Mandatory = $true)]
-    [ValidateSet("Base", "choco")]
+    [ValidateSet("Base", "choco", "about")]
     [string]$buildMode
 )
 
@@ -27,13 +27,11 @@ Switch ($buildMode) {
     'Base' {
         # Build the Docker image
         docker buildx build -t phellams-automator:localbuild -f phellams-automator.dockerfile .
-        # test the image output with a simple command
-        docker run phellams-automator:localbuild pwsh -c get-module -list
         # push to proget - Currently Accessing proget via password passed in via stdin fails
         # normal login works however: docker login -u user
         # --------------
         # $ENV:PROGET_API_KEY | docker login -u admin --password-stdin $ENV:PROGET_HOST
-        # # tag with latest
+        # # tag with latestabout
         # docker tag phellams-psm-builder $ENV:PROGET_HOST/docker/phellams-psm-builder:latest
         # # tag with gitautoversion
         # docker tag phellams-psm-builder $ENV:PROGET_HOST/docker/phellams-psm-builder:$semver
@@ -55,6 +53,16 @@ Switch ($buildMode) {
         # docker tag phellams-psm-builder sgkens/phellams-psm-builder:latest
         # docker push sgkens/phellams-psm-builder:latest
         # docker logout
+    }
+    'about' {
+        # Test image output with get-about
+        docker run phellams-automator:localbuild pwsh -c get-about
+        # test the image output with a simple command
+        docker run phellams-automator:localbuild pwsh -c get-module -list
+    }
+    'clean-up' {
+        # remove all local images matching the tag 'phellams-automator:localbuild'
+        docker rmi $(docker images -q | grep 'phellams-automator:')
     }
     default {
         [console]::write("Please choose a valid build mode: choco or Base`n")
