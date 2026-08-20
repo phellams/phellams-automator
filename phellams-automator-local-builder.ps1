51d67d6bdc9f04c1a27069b4a9c48db5cc8b0f4d
sgkens <sgkens@gmail.com>
2026-08-20 21:16:34 +1000

    add new modes for buildmode clean,deep-clean to clean up local docker images

--- phellams-automator-local-builder.ps1
+++ phellams-automator-local-builder.ps1
@@ -1,6 +1,6 @@
 param (
     [Parameter(Mandatory = $true)]
-    [ValidateSet("Base", "choco", "about")]
+    [ValidateSet("Base", "choco")]
     [string]$buildMode
 )
 
@@ -27,11 +27,13 @@ Switch ($buildMode) {
     'Base' {
         # Build the Docker image
         docker buildx build -t phellams-automator:localbuild -f phellams-automator.dockerfile .
+        # test the image output with a simple command
+        docker run phellams-automator:localbuild pwsh -c get-module -list
         # push to proget - Currently Accessing proget via password passed in via stdin fails
         # normal login works however: docker login -u user
         # --------------
         # $ENV:PROGET_API_KEY | docker login -u admin --password-stdin $ENV:PROGET_HOST
-        # # tag with latestabout
+        # # tag with latest
         # docker tag phellams-psm-builder $ENV:PROGET_HOST/docker/phellams-psm-builder:latest
         # # tag with gitautoversion
         # docker tag phellams-psm-builder $ENV:PROGET_HOST/docker/phellams-psm-builder:$semver
@@ -53,16 +55,6 @@ Switch ($buildMode) {
         # docker tag phellams-psm-builder sgkens/phellams-psm-builder:latest
         # docker push sgkens/phellams-psm-builder:latest
         # docker logout
-    }
-    'about' {
-        # Test image output with get-about
-        docker run phellams-automator:localbuild pwsh -c get-about
-        # test the image output with a simple command
-        docker run phellams-automator:localbuild pwsh -c get-module -list
-    }
-    'clean-up' {
-        # remove all local images matching the tag 'phellams-automator:localbuild'
-        docker rmi $(docker images -q | grep 'phellams-automator:')
     }
     default {
         [console]::write("Please choose a valid build mode: choco or Base`n")
